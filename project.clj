234cbff9cb893e30620e0b0fc620fb8c1b6a63da
Logan Campbell <colin@logaan.net>
2013-04-06 20:29:25 +1100

    added cljsbuild

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-cljsbuild "0.3.0"]] 
+  :cljsbuild {
+    :builds [{:source-paths ["src-cljs"]
+              :compiler {:output-to "resources/public/js/main.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}]})
+
