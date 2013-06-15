2b6c0621ac6afe421dd64ac41cb44d0ce9039760
Logan Campbell <colin@logaan.net>
2013-06-15 23:45:36 +1000

    throwing events into an atom

--- src-cljs/event_thread/main.cljs
+++ src-cljs/event_thread/main.cljs
@@ -1,9 +1,26 @@
 (ns event-thread.main
-  (:require [event-thread.view-models.person :as person]
-            [knockout :as ko]))
+  (:use [jayq.core :only [$ on]]))
 
-(def logan 
-  (person/create :name "Logan" :age "25"))
+(def body
+  ($ :body))
 
-(ko/apply-bindings logan)
+(def events (atom []))
+
+(defn summarise [event]
+  {:type   (aget event "type")
+   :time   (aget event "timeStamp")
+   :target (aget event "target")})
+
+(add-watch events
+  :watch-change
+    (fn [key events old-val new-val]
+      (js/console.log (clj->js new-val))))
+
+(defn append-to-event-log [event]
+  (swap! events conj (summarise event))
+  false)
+
+(on body "change" append-to-event-log)
+
+(on body "submit" append-to-event-log)
 
--- src-cljs/knockout.cljs
+++
@@ -1,6 +0,0 @@
-(ns knockout)
-
-(def observable (.-observable js/ko))
-
-(def apply-bindings (.-applyBindings js/ko))
-
